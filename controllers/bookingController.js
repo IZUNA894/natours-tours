dd753349d1d66ae8f6da59a701ef84a1812ee9f4
IZUNA894 <48329138+IZUNA894@users.noreply.github.com>
2020-07-13 00:30:20 +0530

    bug fixes

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -45,7 +45,7 @@ const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
   const price = session.line_items[0].amount / 100;
-  // await Booking.create({ tour, user, price });
+  await Booking.create({ tour, user, price });
 };
 //creating a checkout (prduction),will called by stripe servers. not by  our app
 module.exports.webhookCheckout = (req, res, next) => {
