6bd3da8f84c2709940427b8b723b5b9b5eaadcef
IZUNA894 <48329138+IZUNA894@users.noreply.github.com>
2020-07-13 18:34:04 +0530

    bugs crashed

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -44,7 +44,7 @@ const createBookingCheckout = async session => {
   console.log("createBooking called", session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 //creating a checkout (prduction),will called by stripe servers. not by  our app
